4dd8f2ee63d0201de5a02ef42a1aaf85ad0faed9
DanyloHet <danylohet@gmail.com>
2026-07-28 14:06:36 -0400

    Created a top level Frontend architecture

---
+++ frontend/eslint.config.js
@@ -0,0 +1,49 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+
+  {
+    files: ['**/*.{js,jsx}'],
+
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+
+    plugins: {
+      react,
+    },
+
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    rules: {
+      'react/jsx-uses-vars': 'error',
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+
+  eslintConfigPrettier,
+])
---
+++ frontend/src/app/router/AppRouter.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router/dom'
+import MainLayout from '../../components/layout/MainLayout.jsx'
+import HomePage from '../../pages/HomePage.jsx'
+import NotFoundPage from '../../pages/NotFoundPage.jsx'
+import { ROUTES } from './routes.js'
+
+const router = createBrowserRouter([
+  {
+    path: ROUTES.HOME,
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '*',
+        element: <NotFoundPage />,
+      },
+    ],
+  },
+])
+
+function AppRouter() {
+  return <RouterProvider router={router} />
+}
+
+export default AppRouter
