e3926a0e09ab0cee2e77d58569fab509c9c01cc1
Mikey Nichols <mnix@journeytocode.io>
2026-08-03 08:39:18 -0400

    Add LoginPage component with fake handshake

--- frontend/src/app/router/AppRouter.jsx
+++ frontend/src/app/router/AppRouter.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router'
 import { RouterProvider } from 'react-router/dom'
+import LoginPage from '../../pages/LoginPage.jsx'
 import MainLayout from '../../components/layout/MainLayout.jsx'
 import HomePage from '../../pages/HomePage.jsx'
 import NotFoundPage from '../../pages/NotFoundPage.jsx'
@@ -7,6 +8,7 @@ import DevComponentsPage from '../../pages/DevComponentsPage.jsx'
 import PrivacyPage from '../../pages/PrivacyPage.jsx'
 import TermsPage from '../../pages/TermsPage.jsx'
 import RegisterPage from '../../pages/RegisterPage.jsx'
+import PasswordResetPage from '../../pages/PasswordResetPage.jsx'
 import { ROUTES } from './routes.js'
 
 const router = createBrowserRouter([
@@ -21,6 +23,14 @@ const router = createBrowserRouter([
       {
         path: ROUTES.REGISTER,
         element: <RegisterPage />,
+      },
+      {
+        path: ROUTES.LOGIN,
+        element: <LoginPage />,
+      },
+      {
+        path: ROUTES.PASSWORD_RESET,
+        element: <PasswordResetPage />,
       },
       {
         path: ROUTES.DEV_COMPONENTS,
