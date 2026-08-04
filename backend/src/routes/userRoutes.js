6e8634a2c30c671986af238fa12318c9e58e0d5f
BytesOfStrength <213146959+BytesOfStrength@users.noreply.github.com>
2026-08-04 00:58:37 -0400

    41/story/mp: Add register, logon, logoff, and password endpoints

---
+++ backend/src/routes/userRoutes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const {
+  register,
+  logon,
+  logoff,
+  verifyEmail,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/userController");
+const jwtMiddleware = require("../middleware/jwtMiddleware");
+const { signupLimiter, loginLimiter } = require("../middleware/rateLimiter");
+
+// POST /api/v1/users/register
+router.post("/register", signupLimiter, register);
+// GET/ verify ? token = (new endpoint for email verification link clicks)
+router.get("/verify", verifyEmail);
+// POST /api/v1/users/login
+router.post("/login", loginLimiter, logon);
+// POST /api/v1/users/logout
+router.post("/logout", jwtMiddleware, logoff);
+
+//forgot password and reset password
+
+// POST /api/v1/users/forgot-password
+router.post("/forgot-password", forgotPassword);
+// POST /api/v1/users/reset-password
+router.post("/reset-password", resetPassword);
+
+module.exports = router;
