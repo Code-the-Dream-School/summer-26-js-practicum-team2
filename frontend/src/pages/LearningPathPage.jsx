2623646b19bf7a4f4f1b97c0bbac02623bd55d12
Kristen Wishart <kwishart24@gmail.com>
2026-08-08 12:28:57 -0400

    updated the LearningPathPage.jsx to import the budgeting.json file.

--- frontend/src/pages/LearningPathPage.jsx
+++ frontend/src/pages/LearningPathPage.jsx
@@ -1,7 +1,7 @@
 // import { useEffect, useState } from 'react'
 import { useEffect, useRef } from 'react'
 // import modules from '../content/index.js'
-import cashFlow from '../content/budgeting.json'
+import cashFlow from '../../../shared/content/budgeting.json'
 import LearningPathNode from '../components/learningPath/LearningPathNode'
 import Card from '../components/ui/Card'
 // import LessonCard from '../components/layout/LessonCard'
