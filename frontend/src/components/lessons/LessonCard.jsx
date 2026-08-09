694497fd380afca5115b1b9c03385dfc3789640f
Kristen Wishart <kwishart24@gmail.com>
2026-08-09 11:45:54 -0400

    added next and back buttons through each microlesson object for users to click through the microless

--- frontend/src/components/lessons/LessonCard.jsx
+++ frontend/src/components/lessons/LessonCard.jsx
@@ -1,10 +1,14 @@
 // import { Link } from 'react-router'
 import { useState } from 'react'
+import { useNavigate } from 'react-router'
 import LessonRenderer from './LessonRenderer'
+import Button from '../ui/Button'
 
 function LessonCard({ lesson, microLesson, onNext }) {
   //Content index state
   const [contentIndex, setContentIndex] = useState(0)
+
+  const navigate = useNavigate()
 
   const content = microLesson.microLessonContent
 
@@ -26,6 +30,17 @@ function LessonCard({ lesson, microLesson, onNext }) {
     setContentIndex((prev) => prev + 1)
   }
 
+  //Helper function for back button
+  //Helper function for back button
+  function handlePreviousContent() {
+    if (contentIndex === 0) {
+      navigate('/learn')
+      return
+    }
+
+    setContentIndex((prev) => prev - 1)
+  }
+
   return (
     <>
       <h1>{lesson.title}</h1>
@@ -33,9 +48,15 @@ function LessonCard({ lesson, microLesson, onNext }) {
 
       <LessonRenderer content={currentContent} />
 
-      <button onClick={handleNextContent}>
-        {contentIndex === content.length - 1 ? 'Next Lesson' : 'Continue'}
-      </button>
+      <div className="mt-6 flex justify-between">
+        <Button variant="secondary" onClick={handlePreviousContent}>
+          {contentIndex === 0 ? 'Learning Path' : 'Back'}
+        </Button>
+
+        <Button variant="primary" onClick={handleNextContent}>
+          {contentIndex === content.length - 1 ? 'Next Lesson' : 'Continue'}
+        </Button>
+      </div>
     </>
   )
 }
