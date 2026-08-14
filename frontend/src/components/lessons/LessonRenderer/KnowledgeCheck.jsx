d41fc0a85c810af2f471031be753545c677ca19f
Mikey Nichols <mnix@journeytocode.io>
2026-08-14 01:02:52 -0400

    Move shared components to a root level folder

--- frontend/src/components/lessons/LessonRenderer/KnowledgeCheck.jsx
+++ frontend/src/components/lessons/LessonRenderer/KnowledgeCheck.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Button from "../../ui/Button";
+import Button from "../shared/Button/Button.component";
 
 function KnowledgeCheck({ content }) {
   const [selectedAnswer, setSelectedAnswer] = useState(null);
