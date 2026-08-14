78779b9a9c575c96910a3f01d2b279f33bbc2438
Mikey Nichols <mnix@journeytocode.io>
2026-08-14 08:20:38 -0400

    Enhance AppRouter route structure

--- frontend/src/app/router/AppRouter.jsx
+++ frontend/src/app/router/AppRouter.jsx
@@ -1,29 +1,63 @@
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
-import MainLayout from "../../components/layout/MainLayout.component.jsx";
-import HomePage from "../../pages/HomePage.jsx";
-import NotFoundPage from "../../pages/NotFoundPage.jsx";
+import { Route, Routes } from "react-router";
 import { ROUTES } from "./routes.js";
+import MainLayout from "../../components/layout/MainLayout.component.jsx";
+import ProtectedRoute from "./ProtectedRoute.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: ROUTES.HOME,
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: "*",
-        element: <NotFoundPage />,
-      },
-    ],
-  },
-]);
+import HomePage from "../../pages/HomePage.jsx";
+import LoginPage from "../../pages/LoginPage.jsx";
+import RegisterPage from "../../pages/RegisterPage.jsx";
+import VerifyEmailPage from "../../pages/VerifyEmailPage.jsx";
+import PasswordResetPage from "../../pages/PasswordResetPage.jsx";
+import DashboardPage from "../../pages/DashboardPage.jsx";
+import LearningPathPage from "../../pages/LearningPathPage.jsx";
+import LessonPage from "../../pages/LearnPage.jsx";
+import LastLessonRedirect from "../../pages/LastLessonRedirect.jsx";
+import PrivacyPage from "../../pages/PrivacyPage.jsx";
+import TermsPage from "../../pages/TermsPage.jsx";
+import NotFoundPage from "../../pages/NotFoundPage.jsx";
 
-function AppRouter() {
-  return <RouterProvider router={router} />;
+export default function AppRouter() {
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path={ROUTES.HOME} element={<HomePage />} />
+        <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+        <Route path={ROUTES.REGISTER} element={<RegisterPage />} />
+        <Route path={ROUTES.VERIFY_EMAIL} element={<VerifyEmailPage />} />
+        <Route path={ROUTES.PASSWORD_RESET} element={<PasswordResetPage />} />
+        <Route path={ROUTES.PRIVACY} element={<PrivacyPage />} />
+        <Route path={ROUTES.TERMS} element={<TermsPage />} />
+
+        <Route
+          path={ROUTES.DASHBOARD}
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path={ROUTES.LEARN}
+          element={
+            <ProtectedRoute>
+              <LearningPathPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path={ROUTES.LAST_LESSON}
+          element={
+            <ProtectedRoute>
+              <LastLessonRedirect />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Public so unauthenticated visitors can preview a sample lesson. */}
+        <Route path={ROUTES.LEARN_LESSON} element={<LessonPage />} />
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  );
 }
-
-export default AppRouter;
