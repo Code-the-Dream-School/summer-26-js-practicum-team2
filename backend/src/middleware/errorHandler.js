b3613256b40f0a345a73502d958d7dcd1260b605
Mikey Nichols <mnix@journeytocode.io>
2026-08-23 12:13:14 -0400

    consume next param in error middleware to satisfy lint

--- backend/src/middleware/errorHandler.js
+++ backend/src/middleware/errorHandler.js
@@ -1,6 +1,10 @@
 const { StatusCodes } = require("http-status-codes");
 
-const errorHandlerMiddleware = (err, req, res, _next) => {
+const errorHandlerMiddleware = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
   console.error(
     "Internal server error: ",
     err.constructor.name,
